c2a68cbe7870cc3bf1d30acb57947abb3bba2330
Torrence T Washington <117210035+twashington04@users.noreply.github.com>
2023-02-15 13:01:16 -0500

    Initial commit

---
+++ src/main/java/Recitation4.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Recitation4 {
+    public static void main(String[] argv)
+    {
+        Scanner scan = new Scanner(System.in);
+
+        // add your code here
+    }
+}
